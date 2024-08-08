2b024d3451417576ff31de53a15c240ee95c3da5
Ryan Aurelio <ryanaurelio@gmail.com>
2024-08-08 16:10:58 +0200

    Implement login

--- registration/views.py
+++ registration/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages, auth
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from profileapp.models import Profile
-# Create your views here.
+
 
 def register(request):
     if request.method == 'POST':
@@ -16,31 +17,40 @@ def register(request):
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, "Email is already exist!")
-                print("email")
                 return redirect('../register/')
             elif User.objects.filter(username=username).exists():
                 messages.info(request, 'Username is already taken!')
-                print("username")
                 return redirect('../register/')
             else:
-                print("aa1")
-                user = User.objects.create_user(username=username, email=email, password=password, first_name=name, last_name=lastname)
-                print("aa2")
+                user = User.objects.create_user(username=username, email=email, password=password, first_name=name,
+                                                last_name=lastname)
                 user.save()
-                print("aa3")
 
                 user_model = User.objects.get(username=username)
                 print(type(user_model))
-                new_profile = Profile.objects.create(user=user_model, username=username, name=name, lastname=lastname, password=password)
+                new_profile = Profile.objects.create(user=user_model, username=username, name=name, lastname=lastname,
+                                                     password=password)
                 new_profile.save()
                 return redirect('../login/')
         else:
-            print("bb")
             messages.info((request, "Passwords do not match"))
             return redirect('register/')
 
     return render(request, 'register.html')
 
 
-def login(request):
+def log_in(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('/')
+        else:
+            messages.info(request, "Incorrect username or password.")
+            return redirect('../login/')
+
     return render(request, 'login.html')
